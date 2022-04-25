2774628fa4fb3aa71991a87e4eefb00c91077865
Amares03 <amaressabit0314@gmail.com>
2022-04-25 18:24:34 +0300

    well

--- pdfService.js
+++ pdfService.js
@@ -5,6 +5,16 @@ function buldPdf() {
   const pdf = new PDFDocument({ size: "A4" });
 
   pdf.pipe(fs.createWriteStream(`output1.pdf`));
+  const fullWidth = 530.28;
+  const fullHeight = 841.89;
+
+  pdf.image(fs.readFileSync(`${__dirname}/assets/header.jpg`), 0, 5, {
+    height: 75,
+    width: fullWidth,
+  });
+  pdf.fontSize(10).fillColor("#000").text("Name", 10, 100);
+
+  pdf.end();
 }
 
 module.exports = { buldPdf };

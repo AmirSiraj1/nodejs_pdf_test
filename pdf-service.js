2b92d071e2c5d3e927a5647cddb16d7a9e01f67b
Amares03 <amaressabit0314@gmail.com>
2022-05-11 21:11:56 +0300

    begin

--- pdf-service.js
+++ pdf-service.js
@@ -7,11 +7,11 @@ function buldPdf() {
   pdf.pipe(fs.createWriteStream(`output1.pdf`));
   const fullWidth = 649.13;
   const fullHeight = 918.43;
-  const nameAxis = 130;
+  const nameAxis = 120;
 
-  pdf.image(fs.readFileSync(`${__dirname}/assets/MBM_logo.png`), 0, 0, {
-    height: 120,
-    width: 110,
+  pdf.image(fs.readFileSync(`${__dirname}/assets/MBM_logo1.png`), 4, 0, {
+    height: 100,
+    width: 185,
   });
   //***************************texts*********************************************** */
   pdf
@@ -176,80 +176,80 @@ function buldPdf() {
 
   //**************LINE*************************************************************** */
   pdf
-    .moveTo(12, 250)
-    .lineTo(fullWidth - 12, 250)
+    .moveTo(12, 240)
+    .lineTo(fullWidth - 12, 240)
     .strokeColor("#000")
     .lineWidth(1.2)
     .stroke();
   //*************************center Text******************************************** */
-  pdf.font("Helvetica-Bold").fontSize(15).text(`Molecular Biology`, 240, 252, {
+  pdf.font("Helvetica-Bold").fontSize(15).text(`Molecular Biology`, 240, 242, {
     width: 180,
     underline: true,
   });
   //*********************square**************************************************** */
   pdf
-    .moveTo(12, 270)
-    .lineTo(fullWidth - 12, 270)
+    .moveTo(12, 260)
+    .lineTo(fullWidth - 12, 260)
     .strokeColor("#000")
     .lineWidth(0.5)
     .stroke();
 
   pdf
-    .moveTo(12, 289)
-    .lineTo(fullWidth - 12, 289)
+    .moveTo(12, 279)
+    .lineTo(fullWidth - 12, 279)
     .strokeColor("#000")
     .lineWidth(1.2)
     .stroke();
   pdf
-    .moveTo(12, 270)
-    .lineTo(12, 650)
+    .moveTo(12, 260)
+    .lineTo(12, 640)
     .strokeColor("#000")
     .lineWidth(0.5)
     .stroke();
   pdf
-    .moveTo(fullWidth - 12, 270)
-    .lineTo(fullWidth - 12, 650)
+    .moveTo(fullWidth - 12, 260)
+    .lineTo(fullWidth - 12, 640)
     .strokeColor("#000")
     .lineWidth(0.5)
     .stroke();
   pdf
-    .moveTo(12, 650)
-    .lineTo(fullWidth - 12, 650)
+    .moveTo(12, 640)
+    .lineTo(fullWidth - 12, 640)
     .strokeColor("#000")
     .lineWidth(0.5)
     .stroke();
   //***********************text************************************************** */
-  pdf.font("Helvetica-Bold").fontSize(10).text(`Test`, 28, 274);
-  pdf.font("Helvetica-Bold").fontSize(10).text(`Result`, 192, 274);
-  pdf.font("Helvetica-Bold").fontSize(10).text(`Reference Range`, 370, 274);
-  pdf.font("Helvetica-Bold").fontSize(10).text(`Methodology`, 493, 274);
+  pdf.font("Helvetica-Bold").fontSize(10).text(`Test`, 28, 264);
+  pdf.font("Helvetica-Bold").fontSize(10).text(`Result`, 192, 264);
+  pdf.font("Helvetica-Bold").fontSize(10).text(`Reference Range`, 370, 264);
+  pdf.font("Helvetica-Bold").fontSize(10).text(`Methodology`, 493, 264);
   //*******************************************Text********************** */
   pdf
     .font("Helvetica-Bold")
     .fontSize(11.5)
-    .text(`COVID-19 by RT-PCR *`, 28, 302);
+    .text(`COVID-19 by RT-PCR *`, 28, 292);
   pdf
     .font("Helvetica-Bold")
     .fontSize(11.5)
-    .text(`Not Detected (Negative)`, 192, 302);
-  pdf.font("Helvetica").fontSize(10).text(`Not Detected`, 370, 302);
-  pdf.font("Helvetica").fontSize(10).text(`Multiplex Real Time PCR`, 493, 302);
-  pdf.font("Helvetica").fontSize(10).text(`Sample Type: `, 28, 330);
+    .text(`Not Detected (Negative)`, 192, 292);
+  pdf.font("Helvetica").fontSize(10).text(`Not Detected`, 370, 292);
+  pdf.font("Helvetica").fontSize(10).text(`Multiplex Real Time PCR`, 493, 292);
+  pdf.font("Helvetica").fontSize(10).text(`Sample Type: `, 28, 320);
   pdf
     .font("Times-Italic")
     .fontSize(10.5)
-    .text(`Nasopharyngeal / Oropharyngeal Swab `, 97, 330);
+    .text(`Nasopharyngeal / Oropharyngeal Swab `, 97, 320);
   pdf
     .font("Helvetica-Bold")
     .fontSize(11.5)
-    .text(`Interpretation Notes :`, 24, 358);
+    .text(`Interpretation Notes :`, 24, 348);
   pdf
     .font("Helvetica-Bold")
     .fontSize(11.5)
     .text(
       `Result                          Interpretation and Recommendations`,
       24,
-      378
+      368
     );
   pdf
     .font("Helvetica")
@@ -257,7 +257,7 @@ function buldPdf() {
     .text(
       `Detected                         If the result is detected, that means the sample is positive for SARS-CoV-2/Covid-19`,
       24,
-      398
+      388
     );
   pdf
     .font("Helvetica")
@@ -265,7 +265,7 @@ function buldPdf() {
     .text(
       `Not Detected                      If the result is not detected, that means the sample is negative for SARS-CoV-2/Covid-19`,
       24,
-      410
+      400
     );
   pdf
     .font("Helvetica")
@@ -273,7 +273,7 @@ function buldPdf() {
     .text(
       `Presumptive Positive     Only one of the multiple genes is detected or a low viral load is possible. Request for repeat sample in 72-96 hours.`,
       24,
-      422,
+      412,
       { width: fullWidth - 25, lineGap: 0.5 }
     );
   pdf
@@ -282,7 +282,7 @@ function buldPdf() {
     .text(
       `The false-negative result with clinical symptoms may be caused by`,
       24,
-      462
+      452
     );
   pdf
     .font("Helvetica")
@@ -290,39 +290,39 @@ function buldPdf() {
     .text(
       `. Unsuitable collection, handling, and/or storage of samples. `,
       24,
-      474
+      464
     );
   pdf
     .font("Helvetica")
     .fontSize(11.5)
-    .text(`. Sample outside of the viremic phase.`, 24, 486);
+    .text(`. Sample outside of the viremic phase.`, 24, 476);
   pdf
     .font("Helvetica")
     .fontSize(11.5)
-    .text(`We recommend repeating testing on a fresh sample`, 24, 498);
+    .text(`We recommend repeating testing on a fresh sample`, 24, 488);
   pdf
     .font("Helvetica-Bold")
     .fontSize(11)
-    .text(`LIMITATION OF THE TEST`, 24, 515);
+    .text(`LIMITATION OF THE TEST`, 24, 505);
   pdf
     .font("Helvetica")
     .fontSize(11.5)
     .text(
       `· This assay has been validated for use with an oropharyngeal swab and nasopharyngeal swab samples only.`,
       24,
-      530
+      520
     );
   pdf
     .font("Helvetica")
     .fontSize(11.5)
-    .text(`· This assay only detects RdRp, N gene, and E gene.`, 24, 542);
+    .text(`· This assay only detects RdRp, N gene, and E gene.`, 24, 532);
   pdf
     .font("Helvetica")
     .fontSize(11.5)
     .text(
       `· This test cannot rule out diseases caused by other pathogens. `,
       24,
-      554
+      544
     );
   pdf
     .font("Helvetica-Bold")
@@ -330,23 +330,23 @@ function buldPdf() {
     .text(
       `This certificate has been issued electronically by MBM Medical Laboratory. Any party that relies on the result of this certificate should first check   its authenticity by scanning of the above QR code or by sending email to reception.Info@mbmlab.ae . MBM Medical laboratory is not responsible for any misuse of this certificate or its content`,
       22,
-      585,
+      575,
       { width: fullWidth - 27.5, lineGap: 0.5 }
     );
-  pdf.font("Helvetica-Bold").fontSize(9).text(`End Of Report `, 260, 639);
-  pdf.font("Helvetica").fontSize(9).text(`Page 1 Of 1 `, 290, 739);
+  pdf.font("Helvetica-Bold").fontSize(9).text(`End Of Report `, 260, 629);
+  pdf.font("Helvetica").fontSize(9).text(`Page 1 Of 1 `, 290, 729);
 
   //****************************************IMAGE************************************** */
-  pdf.image(fs.readFileSync(`${__dirname}/assets/segniture.png`), 10, 689, {
+  pdf.image(fs.readFileSync(`${__dirname}/assets/segniture.png`), 10, 669, {
     height: 95,
     width: 200,
   });
 
-  pdf.image(fs.readFileSync(`${__dirname}/assets/sstamp.png`), 470, 679, {
+  pdf.image(fs.readFileSync(`${__dirname}/assets/sstamp.png`), 470, 659, {
     height: 110,
     width: 110,
   });
-  pdf.image(fs.readFileSync(`${__dirname}/assets/imgs.png`), 10, 827, {
+  pdf.image(fs.readFileSync(`${__dirname}/assets/imgs.png`), 10, 807, {
     height: 95,
     width: 155,
   });
@@ -356,7 +356,7 @@ function buldPdf() {
     .text(
       "THIS IS A SYSTEM GENERATED REPORT AND DOES NOT REQUIRE PHYSICAL SIGNATURE",
       150,
-      815,
+      795,
       {
         width: 400,
       }
@@ -364,33 +364,33 @@ function buldPdf() {
   pdf
     .font("Helvetica")
     .fontSize(8)
-    .text("Printed By:     Automatic Printing", 15, 800, {
+    .text("Printed By:     Automatic Printing", 15, 780, {
       width: 180,
     });
   pdf
     .font("Helvetica")
     .fontSize(8)
-    .text("Printed Date:     28/04/2022 20:46", 474, 800, {
+    .text("Printed Date:     28/04/2022 20:46", 474, 780, {
       width: 150,
     });
   pdf
     .font("Helvetica-Bold")
     .fontSize(8)
     .fillColor("#FF0000")
-    .text("Final Report", 290, 800, {
+    .text("Final Report", 290, 780, {
       width: 150,
     });
-  pdf
-    .font("Helvetica-Bold")
-    .fontSize(8)
-    .text(
-      "Al Quoz,Industrial Area 4 - P.O Box 26148,Dubai,UAE - Tel: +971 55 538 7248-Fax: +971 4 386 9998-Email: customercaredxb@menalabs.com",
-      20,
-      fullHeight - 20,
-      {
-        width: fullWidth,
-      }
-    );
+  // pdf
+  //   .font("Helvetica-Bold")
+  //   .fontSize(8)
+  //   .text(
+  //     "Al Quoz,Industrial Area 4 - P.O Box 26148,Dubai,UAE - Tel: +971 55 538 7248-Fax: +971 4 386 9998-Email: customercaredxb@menalabs.com",
+  //     20,
+  //     fullHeight - 30,
+  //     {
+  //       width: fullWidth,
+  //     }
+  //   );
 
   pdf.end();
 }

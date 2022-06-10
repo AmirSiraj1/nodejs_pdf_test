f8b5e1b8c5cbe094bbdf053a7ed73091f3319847
Amares03 <amaressabit0314@gmail.com>
2022-06-10 20:24:56 +0300

    components

--- pdf-cheker.js
+++ pdf-cheker.js
@@ -1,109 +1,89 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
+const qrCode = require("qrcode");
 
 const pdf = new PDFDocument({ size: "A5", margin: 10 });
-const fullWidth = 595.28;
-const fullHeight = 841.89;
-const nameAxis = 76;
-const textYaxis = 335;
+const nameYAxis = 150;
+const nameXAxis = 80;
 
-function squer(a, b, c, d) {
-  pdf
-    .moveTo(a, textYaxis + c)
-    .lineTo(b, textYaxis + c)
-    .strokeColor("#000")
-    .lineWidth(0.25)
-    .stroke();
-  pdf
-    .moveTo(a, textYaxis + c)
-    .lineTo(a, textYaxis + d)
-    .strokeColor("#000")
-    .lineWidth(0.25)
-    .stroke();
-  pdf
-    .moveTo(b, textYaxis + c)
-    .lineTo(b, textYaxis + d)
-    .strokeColor("#000")
-    .lineWidth(0.25)
-    .stroke();
-  pdf
-    .moveTo(a, textYaxis + d)
-    .lineTo(b, textYaxis + d)
-    .strokeColor("#000")
-    .lineWidth(0.25)
-    .stroke();
-}
-function customerTextFild(text, xPosition, yPosition) {
-  pdf
-    .font("Times-Roman")
-    .fontSize(11)
-    .fillColor("#000")
-    .text(text, xPosition, textYaxis + yPosition);
+function buldQr() {
+  qrCode.toFile(
+    `${__dirname}/assets/rqimage.png`,
+    "Some text other text",
+    {
+      version: 5,
+      width: 90,
+    },
+    function (err) {
+      if (err) throw err;
+      buldPdf();
+    }
+  );
 }
 
 function buldPdf() {
   pdf.pipe(fs.createWriteStream(`output1.pdf`));
 
+  pdf.image(fs.readFileSync(`${__dirname}/assets/checklogo.png`), 0, 0, {
+    height: pdf.page.height,
+    width: pdf.page.width,
+  });
+  pdf.image(
+    fs.readFileSync(`${__dirname}/assets/rqimage.png`),
+    nameXAxis + 150,
+    nameYAxis + 270,
+    {
+      height: 120,
+      width: 120,
+    }
+  );
+
   pdf
-    .font("Times-Bold")
-    .fontSize(8)
+    .font("Helvetica")
+    .fontSize(9)
     .fillColor("#000")
-    .text("1/18/21, 5:20 PM", 25, 20);
-  //***********************first vertical line ----******** */
-  pdf.moveTo(30, 36).lineTo(100, 36).strokeColor("#000").lineWidth(4).stroke();
-  //****************first horizontal line |************ */
-  pdf.moveTo(30, 34).lineTo(30, 400).strokeColor("#000").lineWidth(4).stroke();
-  //**************second vertical line ---********************* */
+    .text("Scan Here ", nameXAxis + 160, nameYAxis + 257);
   pdf
-    .moveTo(30, 400)
-    .lineTo(400, 400)
-    .strokeColor("#000")
-    .lineWidth(4)
-    .stroke();
-  //********************second vertical line***************  */
-  pdf
-    .moveTo(100, 36)
-    .lineTo(100, 400)
-    .strokeColor("#000")
-    .lineWidth(4)
-    .stroke();
-  //********************************image****************** */
-  //   pdf.image(fs.readFileSync(`${__dirname}/assets/head.jpg`), 100, 40, {
-  //     height: 250,
-  //     width: 400,
-  //   });
-  //************************************************** */
+    .font("Helvetica-Bold")
+    .fontSize(20)
+    .fillColor("#0f55eb")
+    .text("Covid Result Certificate ", nameXAxis + 40, 120);
   pdf
     .font("Times-Roman")
-    .fontSize(11)
-    .fillColor("#BFCFFF")
-    .text("Demographic information", 237, textYaxis - 14);
-  //*********************left text********************** */
-  customerTextFild("Full Name : MUSTOFA KASSA BELACHEW", 60, 0);
-  customerTextFild("Id. No : ", 60, 15);
-  customerTextFild("Sex: ", 60, 30);
-  customerTextFild("Region: ", 60, 45);
-  customerTextFild("Woreda: ", 60, 60);
-  customerTextFild("Village/Got: ", 60, 75);
-
-  //**************right text********************* */
-  customerTextFild("Date of birth: 1982-01-01", 313, 0);
-  customerTextFild("Passport Number: ", 313, 15);
-  customerTextFild("Occupation: ", 313, 30);
-  customerTextFild("Zone/sub-city: ", 313, 45);
-  customerTextFild("Kebele: ", 313, 60);
-  customerTextFild("Place of vaccination: Bole 17/20 HC", 313, 75);
-
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Full Name : ", nameXAxis, nameYAxis);
   pdf
-    .moveTo(fullWidth - 50, textYaxis + 110)
-    .lineTo(fullWidth - 50, textYaxis + 170)
-    .strokeColor("#000")
-    .lineWidth(0.25)
-    .stroke();
-  customerTextFild("Prepared by: Helina Embibel", 60, 190);
-  customerTextFild("Authorized by :", 420, 190);
-  customerTextFild("Issued date: 2021-11-18", 60, 207);
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Passport/Kebele ID : ", nameXAxis, nameYAxis + 30);
+  pdf
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Date of Birth : ", nameXAxis, nameYAxis + 60);
+  pdf
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Gender : ", nameXAxis, nameYAxis + 90);
+  pdf
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Nationality : ", nameXAxis, nameYAxis + 120);
+  pdf
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Result : ", nameXAxis, nameYAxis + 150);
+  pdf
+    .font("Times-Roman")
+    .fontSize(15)
+    .fillColor("#0f55eb")
+    .text("Result issued date : ", nameXAxis, nameYAxis + 180);
   pdf.end();
 }
 
-module.exports = { buldPdf };
+module.exports = { buldQr };
